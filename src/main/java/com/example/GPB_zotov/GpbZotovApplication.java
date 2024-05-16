0fd5253644ff522d1a99b114cc99db24188a5e88
savvazot <zotov.savva@gmail.com>
2024-05-16 19:13:06 +0300

    start

---
+++ src/main/java/com/example/GPB_zotov/GpbZotovApplication.java
@@ -0,0 +1,13 @@
+package com.example.GPB_zotov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpbZotovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpbZotovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GPB_zotov/GpbZotovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GPB_zotov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpbZotovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
